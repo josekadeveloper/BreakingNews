b26fe3b8a99697d62f951b8a6b2879da73aa407c
josekadeveloper <josekadeveloper@gmail.com>
2023-09-03 12:01:38 +0200

    feat(main.jsx): change index to sass and add custom height to tailwind

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
+import './index.scss'
 
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore/lite";
