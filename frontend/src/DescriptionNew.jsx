8ee9e76c635f7d243f3a86875e98324ecc037506
josekadeveloper <josekadeveloper@gmail.com>
2023-09-03 12:40:21 +0200

    feat(DescriptionNew): add arrow button to get back home page

--- frontend/src/DescriptionNew.jsx
+++ frontend/src/DescriptionNew.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { getFormatedDay, getTodaySportNews, getTodaySportNewsDescription } from "./functions";
 import Loader from 'react-loaders';
+import arrow from './assets/arrow.png';
 
 const DescriptionNew = () => {
   const { descriptionId } = useParams();
@@ -36,24 +37,28 @@ const DescriptionNew = () => {
     });
   };
 
-
   const getImageSrc = () => {
     return sportNewsDescription?.newsDescriptionData?.[descriptionId]?.image
       ? sportNews?.news?.slice(1)[descriptionId]?.image
       : sportNewsDescription?.newsDescriptionData?.[descriptionId]?.image;
   }
 
+  const goBack = () => {
+    window.history.back();
+  };
+
   return (
     <>
       <div className='max-w-screen-md mx-auto pt-6 pb-6'>
-        <div className="justify-center items-center">
+        <div className='justify-center items-center'>
+          <button onClick={goBack}><img src={arrow} alt='Go Back' className="rotate-90"/></button>
           <h3 className='text-1xl text-red-400 underline mb-4'>{sportNewsDescription?.newsDescriptionData?.[descriptionId]?.category}</h3>
           <h2 className='text-1xl text-yellow-300 mb-4'>{sportNewsDescription?.newsDescriptionData?.[descriptionId]?.title}</h2>
           <h2 className='text-1xl text-white mb-4'>{sportNewsDescription?.newsDescriptionData?.[descriptionId]?.description}</h2>
           <img className='pt-1 max-h-[32rem]' src={getImageSrc()} />
         </div>
       </div>
-      {isLoading && <Loader type='ball-zig-zag-deflect' color="#FDE047" />}
+      {isLoading && <Loader type='ball-zig-zag-deflect' color='#FDE047' />}
     </>
   );
 };
